4e3ccfb2bfe252691265ad98ea8f5524f72e413e
ivanhrt <ivanherreror@gmail.com>
2022-09-30 09:16:02 +0200

    Mi primer commit

---
+++ Test3.java
@@ -0,0 +1,11 @@
+public class Test3 {
+        public static void main(String[] args) {
+            /*System.out.println("Hola");
+            System.out.println("Adios");
+            System.out.println("cucu");*/
+            for(int i=0;i<6;/* ahora viene el paso */ i++)
+            	System.out.println(i+" ");
+            System.out.println("FIN DEL PROGRAMA");
+
+        }
+}
